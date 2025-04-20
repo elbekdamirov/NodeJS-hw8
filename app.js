ef701c70454d1fd51766e0e9a6651cec7716d49c
elbekdamirov <elbekmain@gmail.com>
2025-04-20 11:37:59 +0500

    hw8

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ app.use((req, res) => {
   res.render(createViewPage("404"), { title: "404" });
 });
 
-console.log("hello");
-
 app.listen(PORT, () => {
   console.log(`Server started at: http://localhost:${PORT}`);
 });
